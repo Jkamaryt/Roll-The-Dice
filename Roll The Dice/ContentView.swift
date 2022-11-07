e24aadf8750eac37293f666444ea822561dd3b3e
Jack Kamaryt <jkamaryt46@gmail.com>
2022-11-07 09:52:41 -0600

    1

--- Roll The Dice/ContentView.swift
+++ Roll The Dice/ContentView.swift
@@ -11,11 +11,14 @@ struct ContentView: View {
     @State private var randomValue = 0
     var body: some View {
         VStack {
-           
             Text("Dice Roll").font(title)
             Text("\(randomValue)")
                 .font(.system(size: 72))
                 .padding()
+                .onTapGesture {
+                    randomValue = Int.random(in: 1...6)
+                    Spacer()
+                }
         }
         .padding()
     }
